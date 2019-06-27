e1173a9ed15bc744117c86c476eb6325cc497c46
varangian111 <rrowe222@gmail.com>
2019-06-27 16:46:48 -0500

    Rest of commit lol

--- components/checkin.vue
+++ components/checkin.vue
@@ -1,10 +1,74 @@
 <template>
-	$END$
+	<form id="checkinForm">
+		<div id="customerInformation">
+			<h2>Enter Customer Information</h2>
+			<div id="topRow">
+				<input id="firstName" placeholder="First Name" v-model="firstName">
+				<input id="lastName" placeholder="Last Name" v-model="lastName">
+				<input id="accountNumber" placeholder="Account #" v-model="account">
+			</div>
+			<div id="bottomRow">
+				<input id="address" placeholder="Customer Address" v-model="address">
+				<input id="phoneNumber" placeholder="Phone #" v-model="phone">
+				<input id="email" placeholder="Email address" v-model="email">
+			</div>
+			<div id="computerInformation">
+				<h2>Enter Computer Information</h2>
+				<div>
+					<select id="computerMFG" v-model="computerMFG">
+						<option disabled selected>Select Manufacturer</option>
+						<option :key="index" v-bind:value="MFGOptions.value"
+						        v-for="(MFGOptions, index) in MFGOptions">
+							{{ MFGOptions.text }}
+						</option>
+					</select>
+					<input id="modelNumber" placeholder="Model #" type="text">
+					<input id="serialNumber" placeholder="Serial #" type="text">
+				</div>
+			</div>
+			<div id="repairInformation">
+				<textarea id="problemDescription" placeholder="Describe issues" v-model="problemDescription"></textarea>
+				<textarea id="solution" placeholder="Enter repair information" v-model="solution"></textarea>
+			</div>
+			<div id="accessoryInformation">
+				<label for="powerAdapter">Power Adapter?</label>
+				<input id="powerAdapter" type="checkbox" v-model="powerCord">
+			</div>
+		</div>
+	</form>
 </template>
 
 <script>
 	export default {
-		name: "checkin"
+		name: "checkin",
+		data: function () {
+			return {
+				//Customer Data
+				firstName: "",
+				lastName: "",
+				address: "",
+				phone: "",
+				account: "",
+				email: "",
+				//Computer Data
+				computerMFG: "",
+				MFGOptions: [
+					{text: "Apple", value: "Apple"},
+					{text: "Lenovo", value: "Lenovo"},
+					{text: "HP", value: "HP"},
+					{text: "Gateway", value: "Gateway"},
+					{text: "Toshiba", value: "Toshiba"},
+					{text: "MSI", value: "MSI"},
+					{text: "Alienware", value: "Alienware"},
+					{text: "eMachine", value: "eMachine"},
+					{text: "Other", value: "Other"},
+				],
+				//Repair Data
+				problemDescription: "",
+				solution: "",
+				powerCord: "",
+			}
+		}
 	}
 </script>
 
--- components/pricing.vue
+++ components/pricing.vue
@@ -1,5 +1,14 @@
 <template>
-	$END$
+	<form id="pricingForm">
+		<div>
+			<label for="1hrLaborFee">1hr Labor Fee ($70)</label>
+			<input id="1hrLaborFee" type="checkbox">
+			<label for="benchFee">Bench Fee ($25)</label>
+			<input id="benchFee" type="checkbox">
+			<label for="customFee">Custom Fee</label>
+			<input id="customFee" type="number">
+		</div>
+	</form>
 </template>
 
 <script>
