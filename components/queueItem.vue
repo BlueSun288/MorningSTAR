917f04774093fd90669e613052961eb11fac680c
varangian111 <rrowe222@gmail.com>
2019-09-18 15:21:42 -0500

    🤡🤡🤡🤡🤡🤡🤡

---
+++ components/queueItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <ul>
+      <li>{{serviceOrder}}</li>
+      <li>{{clientName}}</li>
+      <li>{{accountNumber}}</li>
+      <li>{{serviceAddress}}</li>
+      <li>{{inDate}}</li>
+      <li>{{outDate}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "queueItem",
+		props: ["repair"],
+		data: function () {
+			return {
+				clientName: this.repair['Customer Name'],
+				accountNumber: this.repair['Account #'],
+				serviceAddress: this.repair['Service Address'],
+				inDate: this.repair['Date Checked in'],
+				outDate: this.repair['Expected Completion Date'],
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
--- pages/repair_queue.vue
+++ pages/repair_queue.vue
@@ -1,11 +1,16 @@
 <template>
 	<div class="mt-2">
     <form-header :card-subtitle="subtitle" :card-title="title"></form-header>
+    <div id="repairList"></div>
   </div>
+
 </template>
 
 <script>
 	import FormHeader from "../components/formHeader";
+  import firebase from '@/plugins/firebase'
+  import ListItem from '../components/queueItem'
+  import Vue from 'vue'
 
   export default {
 		name: "repair_queue",
@@ -15,7 +20,33 @@
 			  title: "Repair Queue",
 			  subtitle: "Ongoing Repairs"
 		  }
-	  }
+	  },
+      created(){
+			this.fetchRepairs();
+      },
+	  methods :{
+			fetchRepairs(){
+				const database = firebase.firestore();
+				const repairsRef = database.collection('In Progress Repairs');
+		    let allRepairs = repairsRef.get().then(snapshot => {
+			  snapshot.forEach(doc => {
+				  console.log(doc.data());
+				  this.appendToList(doc.data(), doc.id)
+			  });
+		  }).catch(error => {
+			  console.log("error getting documents ", error)
+		  })
+      },
+	    appendToList(repairEntry, docID) {
+		    let ComponentClass = Vue.extend(ListItem);
+		    let instance = new ComponentClass({
+			    propsData: {repairEntry}
+		    });
+		    instance.$mount();
+		    this.$refs.repairList.appendChild(instance.$el);
+	    }
+
+    }
   }
 </script>
 
