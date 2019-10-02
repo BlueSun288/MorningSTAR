73169cb9b4cdead9f9709872b574e88cd2dab747
varangian111 <rrowe222@gmail.com>
2019-10-02 15:44:49 -0500

    updated repair-item component to actually pull the check in date.
    experimenting with new navbar styling

--- components/queueItem.vue
+++ components/queueItem.vue
@@ -4,7 +4,7 @@
       <li class="font-bold">Repair ID : {{repairID}}</li>
       <li>Account #: {{repairData['Account #']}}</li>
       <li>Customer Name: {{repairData['Customer Name']}}</li>
-      <li>Check in date: {{repairData['11/July/2019']}}</li>
+      <li>Check in date: {{repairData['Date Checked in']}}</li>
       <li>Phone Number : {{repairData['Phone #']}}</li>
       <li>Wizard Description of Issue: {{repairData['Wizard Description of Issue']}}</li>
     </ul>
